5b2d25b432c4219b5a4353cdbfe36c0103dde4a2
Logan Franken <loganfranken@gmail.com>
2026-04-25 20:47:06 -0400

    Refinements to get the dogs to drag along each other

--- scenes/Dog.gd
+++ scenes/Dog.gd
@@ -1,10 +1,17 @@
-extends CharacterBody2D
+extends Area2D
 
 var _is_dragging = false
+var _is_mouse_over = false
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		_is_dragging = event.is_pressed()
+		_is_dragging = event.is_pressed() and _is_mouse_over
+
+func _on_mouse_entered() -> void:
+	_is_mouse_over = true
+
+func _on_mouse_exited() -> void:
+	_is_mouse_over = false
 
 func _process(_delta: float) -> void:
 	if _is_dragging:
