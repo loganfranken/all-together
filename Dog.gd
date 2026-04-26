5fa34074d87623e688ac09b145efe882a0c4219e
Logan Franken <loganfranken@gmail.com>
2026-04-25 20:34:06 -0400

    Dogs now snap to a grid when being dragged

--- Dog.gd
+++ Dog.gd
@@ -6,6 +6,8 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		_is_dragging = event.is_pressed()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if _is_dragging:
-		position = get_global_mouse_position()
+		var mouse_position = get_global_mouse_position()
+		position.x = mouse_position.x - (int(mouse_position.x) % 80) + 40
+		position.y = mouse_position.y - (int(mouse_position.y) % 80) + 40
