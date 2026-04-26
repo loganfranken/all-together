609ccc63332d0016ced8ddd29fe545bf5b2af27b
Logan Franken <loganfranken@gmail.com>
2026-04-25 22:04:51 -0400

    Player character can now be generated from a bunch of nodes

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,9 +1,18 @@
 extends CharacterBody2D
 
+const PlayerNode = preload("res://scenes/PlayerNode.tscn")
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+const node_infos = [Vector2i(0, 0), Vector2i(1, 1), Vector2i(2, 1)]
+
+func _ready() -> void:
+	for node_info in node_infos:
+		var player_node = PlayerNode.instantiate()
+		player_node.position.x = node_info[0] * 16
+		player_node.position.y = node_info[1] * 16
+		add_child(player_node)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
