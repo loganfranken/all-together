caf5275510f046e474d2a00e564bd36fbd48937a
Logan Franken <loganfranken@gmail.com>
2026-04-25 20:59:18 -0400

    Dogs will now be snapped to their original position if the drop position overlaps

--- scenes/Dog.gd
+++ scenes/Dog.gd
@@ -2,10 +2,14 @@ extends Area2D
 
 var _is_dragging = false
 var _is_mouse_over = false
+var _original_position = null
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		_is_dragging = event.is_pressed() and _is_mouse_over
+		if event.is_pressed() and _is_mouse_over:
+			_start_dragging()
+		elif _is_dragging:
+			_stop_dragging()
 
 func _on_mouse_entered() -> void:
 	_is_mouse_over = true
@@ -18,3 +22,20 @@ func _process(_delta: float) -> void:
 		var mouse_position = get_global_mouse_position()
 		position.x = mouse_position.x - (int(mouse_position.x) % 80) + 40
 		position.y = mouse_position.y - (int(mouse_position.y) % 80) + 40
+	else:
+		modulate = Color.WHITE
+		
+func _start_dragging():
+	_is_dragging = true
+	_original_position = position
+	modulate = Color.YELLOW
+	
+func _stop_dragging():
+	_is_dragging = false
+	
+	print(get_overlapping_areas())
+	if has_overlapping_areas():
+		position = _original_position
+	
+	_original_position = null
+	modulate = Color.WHITE
